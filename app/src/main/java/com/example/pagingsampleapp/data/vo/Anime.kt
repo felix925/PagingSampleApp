f0c22952226de45c3dcf01f22a755af45e36a9f2
felix925 <feli0x10@gmail.com>
2021-11-28 21:31:45 +0900

    feature / DI

---
+++ app/src/main/java/com/example/pagingsampleapp/data/vo/Anime.kt
@@ -0,0 +1,52 @@
+package com.example.pagingsampleapp.data.vo
+
+import com.google.gson.annotations.SerializedName
+import java.io.Serializable
+
+data class AnimeSearchResult (
+    @SerializedName("works")
+    val animeList: List<Anime>,
+    val total_count: Int,
+    val next_page: Int,
+    val prev_page: Int?
+)
+
+data class Anime(
+    val id: Int,
+    val isLiked: Boolean = false,
+    val episodes_count: Int,
+    val images: Images,
+    val mal_anime_id: String,
+    val media: String,
+    val media_text: String,
+    val official_site_url: String,
+    val released_on: String,
+    val released_on_about: String,
+    val season_name: String?,
+    val season_name_text: String?,
+    val syobocal_tid: String,
+    val title: String,
+    val title_kana: String,
+    val twitter_hashtag: String,
+    val twitter_username: String,
+    val watchers_count: Int,
+    val wikipedia_url: String
+): Serializable {
+    data class Facebook(
+        val og_image_url: String?
+    ): Serializable
+
+    data class Images(
+        val facebook: Facebook,
+        val recommended_url: String,
+        val twitter: Twitter
+    ): Serializable
+
+    data class Twitter(
+        val bigger_avatar_url: String?,
+        val image_url: String?,
+        val mini_avatar_url: String?,
+        val normal_avatar_url: String?,
+        val original_avatar_url: String?
+    ): Serializable
+}
---
+++ app/src/main/java/com/example/pagingsampleapp/di/DataModule.kt
@@ -0,0 +1,42 @@
+package com.example.pagingsampleapp.di
+
+import com.example.pagingsampleapp.data.ReposService
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DataModule {
+    @Provides
+    @Singleton
+    fun provideGson(): Gson = GsonBuilder().create()
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient =
+        OkHttpClient.Builder()
+            .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideRetrofitClient(gson: Gson, client: OkHttpClient ): Retrofit =
+        Retrofit.Builder().client(client)
+            .baseUrl("https://api.annict.com/")
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideReposService(retrofit: Retrofit): ReposService =
+        retrofit.create(ReposService::class.java)
+}
