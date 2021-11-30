f6d959df1f5cc4b30e3057a83c5ca6daea83aac1
felix925 <feli0x10@gmail.com>
2021-12-01 00:55:31 +0900

    rename / pager function

--- app/src/main/java/com/example/pagingsampleapp/presentation/PagingSourceViewModel.kt
+++ app/src/main/java/com/example/pagingsampleapp/presentation/PagingSourceViewModel.kt
@@ -2,7 +2,10 @@ package com.example.pagingsampleapp.presentation
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.ExperimentalPagingApi
 import androidx.paging.PagingData
+import androidx.paging.cachedIn
 import androidx.paging.liveData
 import com.example.pagingsampleapp.data.AnimeRepository
 import com.example.pagingsampleapp.data.vo.Anime
@@ -10,15 +13,16 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
+@ExperimentalPagingApi
 @HiltViewModel
 class PagingSourceViewModel @Inject constructor(
     private val animeRepository: AnimeRepository
 ): ViewModel() {
     // Flowの場合
     fun getAnimesAsFlow(): Flow<PagingData<Anime>> =
-        animeRepository.getAnimes().flow
+        animeRepository.getAnime().flow.cachedIn(viewModelScope)
 
     // LiveDataの場合
     fun getAnimesAsLiveData(): LiveData<PagingData<Anime>> =
-        animeRepository.getAnimes().liveData
+        animeRepository.getAnime().liveData
 }
