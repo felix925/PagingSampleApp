4c8f09af5e6b6d48f385892161bac1a6ac86289f
felix925 <feli0x10@gmail.com>
2021-12-01 01:00:52 +0900

    Repositoryの実装

--- app/src/main/java/com/example/pagingsampleapp/data/AnimeRepository.kt
+++ app/src/main/java/com/example/pagingsampleapp/data/AnimeRepository.kt
@@ -1,23 +1,41 @@
 package com.example.pagingsampleapp.data
 
+import androidx.paging.ExperimentalPagingApi
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
+import com.example.pagingsampleapp.data.dao.AnimeDao
+import com.example.pagingsampleapp.data.dao.PageKeyDao
 import com.example.pagingsampleapp.data.vo.Anime
+import java.util.*
 import javax.inject.Inject
 
+@ExperimentalPagingApi
 class AnimeRepository @Inject constructor(
-    private val api: AnimeApi,
-    private val service: AnimeService,
+    private val animeService: AnimeService,
+    private val animeDao: AnimeDao,
+    private val pageKeyDao: PageKeyDao
 ) {
     // DBをつかわないバージョン
-    fun getAnimes(): Pager<Int, Anime> =
-        Pager(
+    fun getAnime(): Pager<Int, Anime> {
+        val sessionId = UUID.randomUUID().toString()
+
+        return Pager(
             config = PagingConfig(
-                pageSize = 20,
-                prefetchDistance = 80,
+                pageSize = 50,
+                prefetchDistance = 100,
                 enablePlaceholders = false,
             ),
-            pagingSourceFactory = {},
+            remoteMediator = AnimeMediator(
+                sessionId = sessionId,
+                animeService = animeService,
+                animeDao = animeDao,
+                pageKeyDao = pageKeyDao
+            ),
+            pagingSourceFactory = {
+                animeDao.getAnimePagingSource(sessionId)
+            },
         )
+    }
+
 
 }
